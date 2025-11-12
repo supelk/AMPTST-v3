97ee89ec92a791ef6ca62c41c71f2e7a3b5e050e
1080ti of the lab <eastB102.edu.stu.cn>
2025-11-12 11:28:59 +0800

    1080ti-11/12-2

---
+++ scripts/V3/AMPTST-v3/ablation/do.sh
@@ -0,0 +1,5 @@
+# for h57 96-
+#bash .//scripts//V3//AMPTST-v3//ablation//onlyeriod.sh
+#bash .//scripts//V3//AMPTST-v3//ablation//onlyfreqency.sh
+bash .//scripts//V3//AMPTST-v3//ablation//onlyeriod-p.sh
+bash .//scripts//V3//AMPTST-v3//ablation//onlyfreqency-p.sh
